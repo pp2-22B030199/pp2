3cf8282cf94a5a3cb849ffd2a961d853a259f687
pp2 lab 1 <sjazira2002@gmai.com>
2023-02-07 15:53:00 +0600

    Add lab3

---
+++ Tsis3/func1/4.py
@@ -0,0 +1,20 @@
+lst = map(int, input().split())
+
+def isprime(x):
+    x = int(x)
+    if x <= 1:
+        return False
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+
+    return True
+def filter_prime(lst):
+    x = []
+    for i in lst:
+        if isprime(i):
+            x.append(i)
+    return x
+
+
+print(filter_prime(lst))
